3b3e24f81a769636d1f263598ad4a86c9a690f01
Raxipaxi <razielferundir@gmail.com>
2016-04-20 00:53:11 -0300

    Clases casi creadas

--- src/PrendaTemporada.java
+++ src/PrendaTemporada.java
@@ -1,7 +1,16 @@
+import java.util.Vector;
+
 /**
  * Created by Raziel Ferundir on 19-Apr-16.
  */
 public class PrendaTemporada extends  Prenda {
     private String estacion;
     private float porcentaje;
+
+    public PrendaTemporada(int codigo, String nombre, Vector<ItemMaterial> itemMateriales,
+                           int cantidad, String estacion, float porcentaje) {
+        super(codigo, nombre, itemMateriales, cantidad);
+        this.estacion = estacion;
+        this.porcentaje = porcentaje;
+    }
 }
